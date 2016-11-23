cc738763f4df1118d0f22ef812157aeb80611992
Gujgiczer Anna <gujgiczer.anna@gmail.com>
2016-11-23 21:45:23 +0100

    Fixed packing algorithm.

--- MI_nagyHF3/src/genetic/BackPack.java
+++ MI_nagyHF3/src/genetic/BackPack.java
@@ -27,10 +27,16 @@ public class BackPack {
 					if (matrix[i][j] == 0) {
 						// item fits in
 						if (height-i >= currItem.height && width-j >= currItem.width) {
+							int checkSum = 0; // cheks if there is 0 everywhere
 							for (int itemI = i; itemI < i + currItem.height; itemI++)
 								for (int itemJ = j; itemJ < j + currItem.width; itemJ++)
-									matrix[itemI][itemJ] = number + 1; // because the index starts with 0
-							break searchPlace;
+									checkSum += matrix[itemI][itemJ];
+							if (checkSum == 0) {
+								for (int itemI = i; itemI < i + currItem.height; itemI++)
+									for (int itemJ = j; itemJ < j + currItem.width; itemJ++)
+										matrix[itemI][itemJ] = number + 1; // because the index strats with 0
+								break searchPlace;
+							}
 						}
 					}
 		}
