5413ceffa7a6e86584968b7a27a8cf0519ecc9c2
Gujgiczer Anna <gujgiczer.anna@gmail.com>
2016-11-23 19:54:40 +0100

    Implemented order crossover.

--- MI_nagyHF3/src/genetic/Entity.java
+++ MI_nagyHF3/src/genetic/Entity.java
@@ -1,5 +1,6 @@
 package genetic;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -37,9 +38,62 @@ public class Entity {
 		genotype.add(index2, element);
 	}
 	
+	// order crossover
 	public Entity crossover(Entity otherParent){
-		// TODO
-		return null;
+		int index1 = new Random().nextInt(genotype.size());
+		int index2 = new Random().nextInt(genotype.size()-index1) + index1;
+		
+		ArrayList<Integer> childGen = new ArrayList<>();
+
+		// matching section
+		for (int i = index1; i < index2; i ++)
+			childGen.add(genotype.get(i));
+		
+		ArrayList<Integer>remained = new ArrayList<>(otherParent.genotype);
+		rotateLeft(remained, index1);
+		remained.removeIf(
+				element -> {return childGen.contains(element); });	// itt lehet hiba
+		
+		for (int i = 0; i < remained.size(); i++)
+			childGen.add(remained.get(i));
+		rotateRight(childGen, index1);
+		
+		Entity child = new Entity(fenotype.width, fenotype.height, items, childGen);
+		return child;
+	}
+	
+	private static <T> ArrayList<T> rotateRight(ArrayList<T> aL, int shift)
+	{
+	    if (aL.size() == 0)
+	        return aL;
+
+	    T element = null;
+	    for(int i = 0; i < shift; i++)
+	    {
+	        // remove last element, add it to front of the ArrayList
+	        element = aL.remove( aL.size() - 1 );
+	        aL.add(0, element);
+	    }
+
+	    return aL;
+	}
+	
+	private static <T> ArrayList<T> rotateLeft(ArrayList<T> aL, int shift)
+	{
+	    if (aL.size() == 0)
+	        return aL;
+
+	    T element = null;
+	    for(int i = 0; i < shift; i++)
+	    {
+	        // remove first element, add it to the end of the ArrayList
+	        element = aL.remove(0);
+	        aL.add(element);
+	    }
+
+	    return aL;
 	}
 	
 }
+
+
